94bca83af8681e5fb1037f97d99c53e8ce7f24fe
JuliKroiter <kroiter09@gmail.com>
2016-11-11 15:28:38 +0200

    Promot video

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Flatservice4you
     config.i18n.load_path += Dir[Rails.root.join('lib', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:en, :pl]
     config.i18n.default_locale = :en
+    config.assets.paths << "#{Rails.root}/app/assets/video"
+    # config.assets.enabled = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
