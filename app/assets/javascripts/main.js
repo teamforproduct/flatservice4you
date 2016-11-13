03eca4bb0460ce2a7b9357111834e89cdf76eb7d
JuliKroiter <kroiter09@gmail.com>
2016-11-13 14:45:33 +0200

    Scroll menu

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -173,10 +173,12 @@ $(function () {
 			var $menu = $('#template-nt-menu');
 			if ( $(window).scrollTop() > 150 ) {
 				$menu.addClass('sleep');
+        $menu.find('img').hide();
 			}
 
 			if ( $(window).scrollTop() < 500 ) {
 				$menu.removeClass('sleep');
+        $menu.find('img').show();
 				$('#template-nt-offcanvass ul li').removeClass('active');
 				$('#template-nt-offcanvass ul li').first().addClass('active');
 			}
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,19 +1,2 @@
 module HomeHelper
-  def home_divider
-    content_tag :div, id: 'home-divider' do
-      content_tag :div, class: "container" do
-        content_tag :div, class: "row" do
-          content_tag :div, class: "col-md-3 col-sm-6 col-xs-6 pull-right" do
-            content_tag :div, class: "menu-button" do
-              link_to "#", class: "js-template-nt-nav-toggle template-nt-nav-toggle",
-                      data: { toggle:"collapse", target: "#template-nt-navbar"},
-                      aria: {expanded: "false", controls:"navbar"}  do
-                content_tag(:span, t('menu_button')) + fa_icon('bars')
-              end
-            end
-          end
-        end
-      end
-    end.html_safe
-  end
 end
