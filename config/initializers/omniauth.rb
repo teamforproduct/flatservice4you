834e721332ca60b1d156ccb046b4e9b273c2a46c
kruglyjmax <kruglyj.max@gmail.com>
2017-06-08 00:44:31 +0300

    Fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.fb_app_id, Rails.application.secrets.fb_secret,
-           {scope: "email", info_fields: 'first_name,last_name,location,gender,link'}
+           {scope: "email", info_fields: 'first_name,last_name,location,gender,link,email'}
 
   configure do |config|
         config.failure_raise_out_environments = []
